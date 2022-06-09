db4c106f4857a3c34af813ad421b86b59c25bea7
Tong <731557539@qq.com>
2022-06-09 16:00:08 +0800

    2-9 Integrated hot deployment

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    public String testHello;
+
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 }
