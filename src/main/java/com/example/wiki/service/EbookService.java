fe8553a303b07c78c7c6ce1c6a146fe4ab316b21
Tong <731557539@qq.com>
2022-06-24 10:02:32 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.example.wiki.domain.EbookExample;
 import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookResp;
+import com.example.wiki.util.CopyUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,13 +25,14 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook :
-                ebookList) {
-            EbookResp ebookResp=new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+        //List<EbookResp> respList=new ArrayList<>();
+//        for (Ebook ebook :
+//                ebookList) {
+//            EbookResp ebookResp=new EbookResp();
+//            BeanUtils.copyProperties(ebook,ebookResp);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> respList = CopyUtils.copyList(ebookList, EbookResp.class);
         return respList;
     }
 }
