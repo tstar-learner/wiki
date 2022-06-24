f1134d42750c133176600196c1ebe045a7eb5e2d
Tong <731557539@qq.com>
2022-06-24 15:44:23 +0800

    5-2 制作the-header the-footer 删除Helloworld 集成axios

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    甲蛙电子书111
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+  props: {
+    msg: String,
+  },
+});
+</script>
